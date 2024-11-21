167820041ed3636b0e151d7a341caf2158288986
SanduniWathsalya <sanduniwathsalya0620@gmail.com>
2024-11-21 14:22:31 +0530

    Update Hero.js

--- src/Pages/AboutUs/components/Hero.js
+++ src/Pages/AboutUs/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       
       <div className="hero-content2">
        <div className="hero-image2">
-          <img src="OIP.jpeg" alt="Web Development" />
+          <img src="about_hero.jpeg" alt="Web Development" />
         </div>
       
         <div className="hero-text2">
